3d2a3002e922c8cfb81a0be7e81ac4ed8f4b05ff
Udi <dnel0605@gmail.com>
2024-12-05 23:14:04 +0900

    item effect
    point light 추가 및 아이템 애니메이션 추가

--- PROJECT_2024_11_12/Assets/Scripts/GamePlay/IngredientItem.cs
+++ PROJECT_2024_11_12/Assets/Scripts/GamePlay/IngredientItem.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 using static UnityEditor.Experimental.GraphView.GraphView;
@@ -24,6 +25,35 @@ public class IngredientItem : Item, IPlayerSensor
 		EnterPlayer();
 	}
 
+
+	IEnumerator ItemAnim()
+	{
+		float time = 0f;
+		float startY = transform.position.y;
+		float endY = startY + 0.3f;
+		bool isAscending = true;
+
+		while (_collider.enabled)
+		{
+			if (_collider.enabled == false)
+				yield break;
+
+			time += (isAscending ? 1 : -1) * Time.deltaTime;
+
+			if (time >= 1f || time <= 0f)
+				isAscending = !isAscending;
+
+			transform.position = new Vector3(
+			    transform.position.x,
+			    Mathf.Lerp(startY, endY, time),
+			    transform.position.z
+			);
+
+			yield return null;
+		}
+
+	}
+
 	public void OnHand() 
 	{
 		_collider.enabled = false;
@@ -32,7 +62,7 @@ public class IngredientItem : Item, IPlayerSensor
 	{
 		transform.SetParent(null);
 		_collider.enabled = true;
-
+		StartCoroutine(ItemAnim());
 	}
 
 	public void InitDungeon()
